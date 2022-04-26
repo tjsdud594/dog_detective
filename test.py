9516fb821eb538e9a5c6302247959724a3da821c
류선영 <84279479+tjsdud594@users.noreply.github.com>
2022-04-26 16:43:40 +0900

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pymysql
-
-
-conn = pymysql.connect(host='localhost', user='root', password='1234', db='meong_tamjung', charset='utf8')
-cursor = conn.cursor()
-cursor.execute("delete from user where name='류선영'")
